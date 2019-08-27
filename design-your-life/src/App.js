0db8521656bb028b3990e44ed9765f8504e66685
Kayla Dailey <kayladailey000@gmail.com>
2019-08-26 20:19:38 -0500

    Started over

--- design-your-life/src/App.js
+++ design-your-life/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import JournalForm from './JournalComponents/JournalForm';
+import styled from 'styled-components'
+
+const WrapperDiv = styled.div`
+    text-align:center;
+    border: black;
+    width: 100%;
+    height: 100%;
+    border-bottom: black;
+`;
+
 
 function App() {
   return (
-    <div className="App">
+    <WrapperDiv>
   <h1> Design Your Life </h1>
      <h3>Activity Log</h3>
      <JournalForm/>
+     </ WrapperDiv>
 
-     
-   
-    </div>
   );
 }
 
--- design-your-life/src/JournalComponents/JournalForm.js
+++ design-your-life/src/JournalComponents/JournalForm.js
@@ -2,25 +2,39 @@ import React from 'react';
 import {Formik, Form, Field} from 'formik'; 
 import { useState } from "react";
 import { Input, Button, Label, Icon} from 'semantic-ui-react';
+import styled from 'styled-components'
+
+
+// const Button = styled.button`
+// padding:6px , 10px;
+// margin: 5px;
+// border: none;
+// border-radius: 3px;
+// color: white;
+// ${props => (props.type === 'primary') ? `background: #2196f3` : null}
+
+
+
 
 function JournalForm() {
    return (
 
 <div className="Mood">
-  <Button class="ui positive basic button">Content</Button>
+  <Button class="ui positive basic button">  Content </Button>
   <Button class="ui yellow basic button">Indifferent</Button>
   <Button class="ui negative basic button">Dissatisfied</Button>
 
 
 <div className="Form">
   <Form>
-    <Label> Activity:<Input type="text"  /></Label>
+    <Label> Activity:<Input type="text" placeholder="Activity"/></Label>
     <textarea name="body" placeholder="Write Your Thoughts Here"/>
   </Form>
 </div>
 
 <div className='SubmitButton'>
   <Button>Log Reflection</Button>
+
 </div>
 
 </div>

254f8ac80c07c101da335f668fb4888bc2af7251
Kayla Dailey <kayladailey000@gmail.com>
2019-08-28 21:57:49 -0500

    moving over code

--- design-your-life/src/App.js
+++ design-your-life/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import JournalForm from './JournalComponents/JournalForm';
+import styled from 'styled-components'
+import React, { Component } from 'react';
 import './App.css';
+
+
+const WrapperDiv = styled.div`
+    text-align:center;
+    border: black;
+    width: 100%;
+    height: 100%;
+    border-bottom: black;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <WrapperDiv>
+    <h1>  </h1>
+      
+      <JournalForm/>
+    </ WrapperDiv>
+
+    );
+  }
 
 export default App;
